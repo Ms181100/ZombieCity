92051959e9f06c52991f5447d876f09e19adf0d2
Ms181100 <94699960+Ms181100@users.noreply.github.com>
2026-09-01 20:41:53 +0000

    Startfehler reparieren

--- app/src/main/java/de/matze/zombiecity/MainActivity.java
+++ app/src/main/java/de/matze/zombiecity/MainActivity.java
@@ -11,13 +11,7 @@ public class MainActivity extends Activity {
     @Override public void onCreate(Bundle state) {
         super.onCreate(state);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setDecorFitsSystemWindows(false);
-        if (android.os.Build.VERSION.SDK_INT >= 30) {
-            WindowInsetsController c = getWindow().getInsetsController();
-            if (c != null) c.hide(WindowInsets.Type.systemBars());
-        } else {
-            getWindow().getDecorView().setSystemUiVisibility(5894);
-        }
+        getWindow().getDecorView().setSystemUiVisibility(5894);
         setContentView(new GameView(this));
     }
 }
